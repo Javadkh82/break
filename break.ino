0d758e5f2892d8812c5715419659bc3ef7ef87e7
javad_KH <166110182+Javadkh82@users.noreply.github.com>
2024-06-05 19:47:59 +0330

    Add files via upload

---
+++ break.ino
@@ -0,0 +1,19 @@
+
+int ledPin = 13;
+volatile int x = 0;
+
+void setup() {
+  attachInterrupt(0, increment, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(ledPin, LOW);
+  delay(3000);
+  Serial.println(x, DEC);
+}
+void increment() {
+  x++;
+  digitalWrite(ledPin, HIGH);
+}
+
